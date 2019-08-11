95d7d2d4f6c4693207229aa4d427a22fac653cde
Noah Li <lishi@onething.com>
2019-08-11 16:37:33 +0800

    初始化

---
+++ pages/message-center/message-center.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="container">
+		<top-tab-bar 
+		:tabNames="tabNames"
+		:showSlider="true"
+		@onTabClick="onTabClick">
+		</top-tab-bar>
+		<message-list class="list" :messages="currentMessages"></message-list>
+	</view>
+</template>
+
+<script>
+	import TopTabBar from "./top-tab-bar.vue"
+	import messageData from "./message-data.js"
+	import MessageList from "./message-list.vue"
+	
+	export default {
+		components: {
+			TopTabBar,
+			MessageList
+		},
+		data() {
+			return {
+				tabNames: ["代办","已办","待阅","已阅"],
+				messages: [messageData.todo, messageData.done, messageData.unread, messageData.readed],
+				currentMessages: messageData.todo
+			}
+		},
+		methods: {
+			onTabClick(index) {
+				console.log("parent receive on tab click! ");
+				this.$data.currentMessages = this.$data.messages[index];
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
